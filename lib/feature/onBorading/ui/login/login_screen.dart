28f74db1170e4ef8f3017de4ff99c872c65e29c0
abdulrahmanahme <abdelramanahmed00@gmail.com>
2024-08-06 11:24:56 +0300

    - initial Sign up

--- lib/feature/onBorading/ui/login/login_screen.dart
+++ lib/feature/onBorading/ui/login/login_screen.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:the_good_doctor/core/helper/extantion.dart';
 import 'package:the_good_doctor/core/widgets/default_button.dart';
 import 'package:the_good_doctor/feature/login/logic/cubit/login_cubit.dart';
 import 'package:the_good_doctor/feature/onBorading/ui/login/widgets/already_have_an_account_widget.dart';
 import 'package:the_good_doctor/feature/onBorading/ui/login/widgets/login_bloc_listener.dart';
 import 'package:the_good_doctor/feature/onBorading/ui/login/widgets/text_from_field_login_widget.dart';
-
-import '../../../../core/routing/routing.dart';
 import '../../../../core/theming/text_styles.dart';
-import '../../../../core/widgets/text_from_felid_widget.dart';
 import '../../../login/data/model/login_request_body.dart';
 
 class LoginScreen extends StatelessWidget {
