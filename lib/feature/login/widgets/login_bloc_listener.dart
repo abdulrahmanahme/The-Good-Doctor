569fa6bcedd66263d8a8af60c761b981be6b31a3
abdulrahmanahme <abdelramanahmed00@gmail.com>
2024-09-15 16:46:09 +0300

    - Add class Hive cash

--- lib/feature/login/widgets/login_bloc_listener.dart
+++ lib/feature/login/widgets/login_bloc_listener.dart
@@ -6,7 +6,6 @@ import 'package:the_good_doctor/core/helper/extantion.dart';
 import 'package:the_good_doctor/core/routing/routing.dart';
 import 'package:the_good_doctor/core/theming/colors.dart';
 import 'package:the_good_doctor/core/widgets/dialog/dialog_handler.dart';
-import 'package:the_good_doctor/feature/Home/ui/home_screen.dart';
 import 'package:the_good_doctor/feature/login/logic/cubit/login_cubit.dart';
 
 class LoginBlocListener extends StatelessWidget {
