c99a2ee7d9a3a4a1cc90b93650b1e9430c93952a
abdulrahmanahme <abdelramanahmed00@gmail.com>
2025-02-08 21:13:33 +0200

    restore git ignore

---
+++ lib/core/app_const/app_const.dart
@@ -0,0 +1,10 @@
+class AppConst {
+static  String token ='Token';
+static  String saveEmailAndPassword ='save Email And Password';
+static  String password ='password';
+static  String email ='email';
+
+
+
+
+}
